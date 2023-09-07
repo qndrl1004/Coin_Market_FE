f9997e3e89f6893d704cbeb593710aff26106f7f
qndrl1004 <qndrl1004@gmail.com>
2023-09-07 16:57:54 +0900

    replace api3 link commit

---
+++ src/api/cadlestick/Candle-stick.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export const CandleStick: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [responseData, setResponseData] = useState<any | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const options = {
+          method: "GET",
+          url: "https://api.bithumb.com/public/orderbook/BTC_KRW",
+          headers: { accept: "application/json" },
+        };
+
+        const response = await axios.request(options);
+        setResponseData(response.data);
+        setLoading(false);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        setError(error?.message || "An error occurred");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Bithumb API Data</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {responseData && (
+        <pre>
+          <code>{JSON.stringify(responseData, null, 2)}</code>
+        </pre>
+      )}
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
-import { DepositWithdrawalSupportStatusAssets } from "./api/public/Deposit-Withdrawal-support-status-assets";
+import { AskingPrice } from "./api/public/Asking-price";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <DepositWithdrawalSupportStatusAssets />
+    <AskingPrice />
   </React.StrictMode>
 );
