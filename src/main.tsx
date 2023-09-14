f5b61376a4768ce1a868df49ad1048322d7b0e77
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-09-14 19:33:19 +0900

    detail page completed

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import Header from "./components/header/Header";
+import Header from './components/header/Header';
 import Footer from "./components/footer/Footer";
-import { NowPrice } from "./pages/public/Now-price";
+import TradingViewWidget from './pages/public/TradingView-api';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Header />
-    <NowPrice />
+    <TradingViewWidget />
     <Footer />
   </React.StrictMode>
 );
--- src/pages/public/Now-price-assets.tsx
+++ src/pages/public/Now-price-assets.tsx
@@ -1,44 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export const NowPriceAssets: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [responseData, setResponseData] = useState<any | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error] = useState<string | null>(null);
+  const [price, setPrice] = useState<string | null>(null);
+
+  // 데이터를 주기적으로 업데이트할 시간 간격 (밀리초 단위)
+  const updateInterval = 1000; // 1초마다 업데이트
+
+  const fetchData = async () => {
+    try {
+      const options = {
+        method: "GET",
+        url: "https://api.bithumb.com/public/ticker/BTC_KRW",
+        headers: { accept: "application/json" },
+      };
+
+      const response = await axios.request(options);
+      setResponseData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const options = {
-          method: "GET",
-          url: "https://api.bithumb.com/public/ticker/BTC_KRW",
-          headers: { accept: "application/json" },
-        };
-
-        const response = await axios.request(options);
-        setResponseData(response.data);
-        setLoading(false);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        setError(error?.message || "An error occurred");
-        setLoading(false);
-      }
-    };
-
+    // 페이지가 로드될 때 한 번 데이터 가져오기
     fetchData();
+
+    // 주기적으로 데이터 업데이트
+    const intervalId = setInterval(fetchData, updateInterval);
+
+    // 컴포넌트가 언마운트될 때 clearInterval로 인터벌 제거
+    return () => clearInterval(intervalId);
   }, []);
 
-  return (
-    <div>
-      <h1>Bithumb API Data</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {responseData && (
-        <pre>
-          <code>{JSON.stringify(responseData, null, 2)}</code>
-        </pre>
-      )}
-    </div>
-  );
+  // responseData 업데이트 후에 price 값을 설정
+  useEffect(() => {
+    if (responseData) {
+      setPrice(responseData.data.closing_price);
+    }
+  }, [responseData]);
+
+  return price
 };
+
+
