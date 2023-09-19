2062046ee61ddc8ceb9ccf7daf5e8fc98487eb24
LKW9 <sg4582@naver.com>
2023-09-19 15:05:58 +0900

    포트폴리오 페이지 만들기
    포트폴리오 페이지 생성, 에러페이지 수정

--- src/pages/error/404.tsx
+++ src/pages/error/404.tsx
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
   return (
-    <div className='min-h-[600px] flex flex-col justify-center items-center'>
+    <div className='min-h-[1100px] flex flex-col justify-center items-center'>
       <h1 className=' text-6xl font-bold mb-[20px]'>ERROR 404</h1>
       <p>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
     </div>
