32e8862e6e15de0775f42aa211a93e9ebd708b26
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-09-21 16:11:00 +0900

    #35 Header, Now-price-assets, Footer, EmojiBtn, Chat-api has changed for DarkMode and reSize issue

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -37,109 +37,119 @@ export default function Header() {
     <header
       className={`${
         darkMode ? "dark" : "light"
-      } fixed top-0 w-full opacity-100 z-10 md:opacity-100 h-[16%] md:flex md:top-0 md:z-10 md:w-full md:h-[16.5%] `}
+      } fixed top-0 w-full z-40 md:top-0 h-[120px] md:h-[155px] shadow-md shadow-slate-200 opacity-100`}
     >
-      <div className="fixed items-center justify-center w-full md:flex md:items-center md:justify-center">
+      <div className="flex items-center justify-between h-[100%] min-w-[300px] overflow-hidden mx-[20px]">
+        {/* 로고 */}
         <a
           href="/"
-          className="fixed top-0 items-center justify-center mt-[2%] md:flex md:top-0 md:mt-[0.2%] md:items-center md:justify-center"
+          className='md:w-[300px] max-w-[40%]'
         >
           <img
-            src="/test.png"
-            alt="test"
-            className="w-[10vw] md:flex md:w-[10vw] h-[200px] -mt-10 bg-red-500 "
+            src={`${darkMode ? "/header-dark.png" : "/header.png"}`}
+            alt={`${darkMode ? "Footer-dark" : "Footer"}`}
           />
         </a>
 
-        <div className="fixed space-x-4 top-0 mt-4 ml-[65%] md:top-0 md:mt-[3%] md:space-x-8 md:ml-[54.5%] md:flex">
-          <button
-            className="md:text-xl md:cursor-pointer md:hover:text-[#efda7a]"
-            onClick={toggleDarkMode}
-          >
-            {!darkMode && (
+        <div className='flex-1 max-w-[60%] h-[100%] flex flex-col items-end justify-center'>
+          {/* 다크모드 로그인키 */}
+          <div className="flex mt-[30px]">
+            <button
+              className="mr-[40px] md:text-xl md:cursor-pointer md:hover:text-[#efda7a]"
+              onClick={toggleDarkMode}
+            >
+              {!darkMode && (
+                <FontAwesomeIcon
+                  icon={faMoon}
+                  size="lg"
+                  style={{
+                    paddingRight: "7px",
+                  }}
+                />
+              )}
+              {darkMode && (
+                <FontAwesomeIcon
+                  icon={faSun}
+                  size="lg"
+                  style={{
+                    paddingRight: "7px",
+                  }}
+                />
+              )}
+            </button>
+            <button
+              className=" md:cursor-pointer md:hover:underline md:hover:text-[#494949]"
+              onClick={() => {
+                openLoginModal();
+              }}
+            >
               <FontAwesomeIcon
-                icon={faMoon}
-                size="lg"
+                icon={faKey}
                 style={{
                   paddingRight: "7px",
                 }}
               />
-            )}
-            {darkMode && (
-              <FontAwesomeIcon
-                icon={faSun}
-                size="lg"
-                style={{
-                  paddingRight: "7px",
-                }}
+              로그인
+            </button>
+          </div>
+
+          <div className='group flex items-center justify-end m-[20px] mr-0 h-[100px] flex-wrap'>
+              {/* 관심목록 포트폴리오*/}
+            <div className="mb-[8px]">
+              <a
+                href="/favorites"
+                className="mr-[20px] md:cursor-pointer md:hover:underline md:hover:text-[#efda7a]"
+              >
+                <FontAwesomeIcon
+                  icon={faStarHalfAlt}
+                  style={{
+                    color: "#efda7a",
+                    paddingRight: "7px",
+                  }}
+                />
+                관심목록
+              </a>
+              <a
+                href="/portfolio"
+                className="md:cursor-pointer md:hover:underline md:hover:text-[#38bdf8] "
+              >
+                <FontAwesomeIcon
+                  icon={faChartPie}
+                  style={{
+                    color: "#38bdf8",
+                    paddingRight: "7px",
+                  }}
+                />
+                포트폴리오
+              </a>
+            </div>
+
+            {/* 검색창 */}
+            <div className="flex items-center  justify-start ml-[20px] mb-[10px] w-[190px] rounded-lg shadow-sm shadow-slate-200 group-focus-within:shadow-blue-400 hover:shadow-blue-400">
+              <label htmlFor="search" className="ml-[10px]">
+                <FontAwesomeIcon icon={faSearch} style={{ paddingRight: "7px" }} />
+              </label>
+              <input
+                type="text"
+                id="search"
+                placeholder="코인입력"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={handleSearch}
+                value={searchTerm}
+                className="w-[100px] h-[30px] focus:outline-none bg-transparent md:focus:outline-none"
               />
-            )}
-          </button>
-          <button
-            className=" md:cursor-pointer md:hover:underline md:hover:text-[#494949]"
-            onClick={() => {
-              openLoginModal();
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faKey}
-              style={{
-                paddingRight: "7px",
-              }}
-            />
-            로그인
-          </button>
+                <a
+                href={`/search/${searchTerm}`}
+                className="ml-[20px] hover:text-blue-500"
+              >
+                검색
+              </a>
+              
+            </div>
+          </div>
+          
         </div>
-
-        <div className="fixed top-0 mt-[50px] space-x-3 ml-[48%] md:flex md:top-0 md:mt-[5.5%] md:-ml-[50%] md:space-x-2 ">
-          <a
-            href="/favorites"
-            className="md:mt-1  md:cursor-pointer md:hover:underline md:hover:text-[#efda7a]"
-          >
-            <FontAwesomeIcon
-              icon={faStarHalfAlt}
-              style={{
-                color: "#efda7a",
-                paddingRight: "7px",
-              }}
-            />
-            관심목록
-          </a>
-          <a
-            href="/portfolio"
-            className="md:mt-1 md:px-6 md:cursor-pointer md:hover:underline md:hover:text-[#38bdf8] "
-          >
-            <FontAwesomeIcon
-              icon={faChartPie}
-              style={{
-                color: "#38bdf8",
-                paddingRight: "7px",
-              }}
-            />
-            포트폴리오
-          </a>
-        </div>
-
-        <div className="fixed top-0 mt-[88px] ml-[45%] bg-slate-200 md:flex md:bg-slate-200 md:top-0 md:mt-[5.5%] md:ml-[50%]">
-          <label htmlFor="search" className=" md:px-1 md:py-1">
-            <FontAwesomeIcon icon={faSearch} style={{ paddingRight: "7px" }} />
-          </label>
-          <input
-            type="text"
-            id="search"
-            placeholder="코인입력"
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyPress={handleSearch}
-            value={searchTerm}
-            className=" bg-slate-200 focus:outline-none md:py-1 md:rounded md:focus:outline-none md:bg-slate-200"
-          />
-          <a
-            href={`/search/${searchTerm}`}
-            className="-ml-6 hover:text-white md:py-1 md:pr-1 md:bg-slate-200 md:hover:text-white"
-          >
-            검색
-          </a>
-        </div>
+        
       </div>
       <ScrollToTop />
       <LoginModal isModalOpen={isLoginModalOpen} onClose={closeLoginModal} />
--- src/pages/public/Now-price-assets.tsx
+++ src/pages/public/Now-price-assets.tsx
@@ -11,19 +11,21 @@ import { useParams } from "react-router-dom";
 import ChattingWidget from '../../api/Chat-api';
 import TradingViewWidget from '../../api/TradingView-api';
 import { GetCoinPrice } from '../../api/GetCoinPrice-api';
+import { useDarkMode } from '../../context/Dark-mode';
 
 const price = <GetCoinPrice />;
 
 export default function NowPriceAssets() {
   const { currency } = useParams();
   const coinName = currency;
+  const { darkMode } = useDarkMode();
 
   return (
-    <main className='mt-[130px] overflow-x-hidden md:min-h-[1100px]'>
+    <main className='mt-[130px] md:mt-[155px] overflow-x-hidden md:min-h-[1100px]'>
       <div className='w-screen md:flex overflow-x-hidden '>
         {/* 왼쪽창 */}
         <section className='min-w-[300px] p-[20px] border-b-2 border-solid border-grey-200 object-scale-down flex flex-col md:w-[400px] md:border-none '>
-        <div className='w-[100%] p-[10px] md:mb-[20px] shadow-sm	shadow-slate-500 border-1 rounded-md'>
+        <div className={`w-[100%] p-[10px] md:mb-[20px] shadow-md border-1 rounded-md ${darkMode?'shadow-white':'shadow-slate-500'}`}>
           <div className=''>
             <div className='flex justify-between'>
               <div>
@@ -41,7 +43,7 @@ export default function NowPriceAssets() {
                 <div className=' flex-1 text-center mr-[7px]'>
                   <div className='group cursor-pointer'>
                     <div className='mx-auto w-[25px] bg-slate-200	rounded-md overflow-hidden transition-all duration-500 group-hover:bg-[#efda7a] group-hover:opacity-100 opacity-70'>
-                      <button><FontAwesomeIcon icon={faStar} size="sm" /></button>
+                      <button><FontAwesomeIcon icon={faStar} size="sm" className='text-black'/></button>
                     </div>
                   </div>
                 </div>
@@ -49,7 +51,7 @@ export default function NowPriceAssets() {
                   <div>
                     <div className='group cursor-pointer'>
                       <div className='mx-auto w-[25px] bg-slate-200	rounded-md overflow-hidden transition-all duration-500 group-hover:bg-[#efda7a] group-hover:opacity-100 opacity-70'>
-                        <button><FontAwesomeIcon icon={faShareNodes} size="sm"/></button>
+                        <button><FontAwesomeIcon icon={faShareNodes} size="sm" className='text-black'/></button>
                       </div>
                     </div>
                   </div>
@@ -64,8 +66,8 @@ export default function NowPriceAssets() {
                   <button className='bg-slate-200 w-[100%] h-[100%] transition-opacity duration-500 group-hover:opacity-100 opacity-60'>
                     <div className='flex justify-between px-[6%]'>
                     <div>
-                      <span ><FontAwesomeIcon icon={faStar} size="sm" /></span>
-                      <label className='ml-[10px]'>관심 목록 추가</label>
+                      <span ><FontAwesomeIcon icon={faStar} size="sm" className='text-yellow-400'/></span>
+                      <label className='ml-[10px] text-black'>관심 목록 추가</label>
                     </div>
                     <div>
                           +
@@ -79,8 +81,8 @@ export default function NowPriceAssets() {
                 <button className='bg-slate-200 w-[100%] h-[100%] transition-opacity duration-500 group-hover:opacity-100 opacity-60'>
                   <div className='flex justify-between px-[6%]'>
                     <div>
-                      <span ><FontAwesomeIcon icon={faChartPie} size="sm" /></span>
-                      <label className='ml-[10px]'>포트폴리오에서 추가</label>
+                      <span ><FontAwesomeIcon icon={faChartPie} size="sm" className='text-blue-400'/></span>
+                      <label className='ml-[10px] text-black'>포트폴리오에서 추가</label>
                     </div>
                     <div>
                           +
