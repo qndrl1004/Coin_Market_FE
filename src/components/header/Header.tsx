34fd3f164d7bf1ae5ed0c01b6c97fcd24fef45b6
qndrl1004 <qndrl1004@gmail.com>
2023-10-25 11:46:43 +0900

    coin fe repair

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,11 +16,11 @@ import LoginBtn from "../button/LoginBtn";
 import { useAuth } from "../../context/IsLogined";
 import { BithumbResponse } from "../../api/TradingChart-api";
 import axios from "axios";
-import LogoutBtn from '../button/LogoutBtn';
+import LogoutBtn from "../button/LogoutBtn";
 
 export default function Header() {
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [responseData, _setResponseData] = useState<BithumbResponse | null>(
+  const [responseData, setResponseData] = useState<BithumbResponse | null>(
     null
   );
   const { darkMode, toggleDarkMode } = useDarkMode();
@@ -62,10 +62,21 @@ export default function Header() {
   };
 
   const sendMessage = () => {
-    if(!isLogin){
+    if (!isLogin) {
       alert("로그인이 필요합니다.");
     }
-  }
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://api.bithumb.com/public/ticker/ALL_KRW", {})
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.error("데이터를 불러오는 중에 오류가 발생했습니다.", error);
+      });
+  }, []);
 
   useEffect(() => {
     axios
@@ -76,7 +87,7 @@ export default function Header() {
         withCredentials: true,
       })
       .then((response) => {
-        setLogin(response.data)
+        setLogin(response.data);
       })
       .catch((error) => {
         console.error(error);
@@ -91,7 +102,10 @@ export default function Header() {
     >
       <LoginModal isModalOpen={isLoginModalOpen} onClose={closeLoginModal} />
       <div className="flex items-center justify-between h-[100%] overflow-hidden md:mx-[20px]">
-        <a href="/" className="min-w-[100px] sm:w-[130px] md:w-[150px] max-w-[40%]">
+        <a
+          href="/"
+          className="min-w-[100px] sm:w-[130px] md:w-[150px] max-w-[40%]"
+        >
           <img
             src={`${darkMode ? "/header-dark.png" : "/header.png"}`}
             alt={`${darkMode ? "Header-dark" : "Header"}`}
@@ -101,23 +115,23 @@ export default function Header() {
         <div className="flex-1 w-full h-[100%] flex">
           <div className="group hidden flex-1 md:flex items-end justify-end mx-[20px] mr-0 flex-wrap">
             <div className="flex flex-col items-center h-full">
-              <div className='w-full md:flex items-center justify-center flex-1 hidden'>
+              <div className="items-center justify-center flex-1 hidden w-full md:flex">
                 <a
-                href={isLogin? "/favorites":""}
-                onClick={sendMessage}
-                className="mr-[20px] md:cursor-pointer md:hover:underline md:hover:text-[#efda7a] w-[120px] text-center"
-              >
-                <FontAwesomeIcon
-                  icon={faStarHalfAlt}
-                  style={{
-                    color: "#efda7a",
-                    paddingRight: "7px",
-                  }}
-                />
-                <span className='text-[14px]'>즐겨찾기</span>
+                  href={isLogin ? "/favorites" : ""}
+                  onClick={sendMessage}
+                  className="mr-[20px] md:cursor-pointer md:hover:underline md:hover:text-[#efda7a] w-[120px] text-center"
+                >
+                  <FontAwesomeIcon
+                    icon={faStarHalfAlt}
+                    style={{
+                      color: "#efda7a",
+                      paddingRight: "7px",
+                    }}
+                  />
+                  <span className="text-[14px]">즐겨찾기</span>
                 </a>
                 <a
-                  href={isLogin? "/portfolio":""}
+                  href={isLogin ? "/portfolio" : ""}
                   onClick={sendMessage}
                   className="md:cursor-pointer md:hover:underline md:hover:text-[#38bdf8] w-[120px] text-center"
                 >
@@ -128,7 +142,7 @@ export default function Header() {
                       paddingRight: "7px",
                     }}
                   />
-                  <span className='text-[14px]'>포트폴리오</span>
+                  <span className="text-[14px]">포트폴리오</span>
                 </a>
               </div>
               <div className=" w-[230px] hidden md:flex items-center rounded-lg shadow-sm shadow-slate-200 group-focus-within:shadow-blue-400 hover:shadow-blue-400 mb-[5px]">
@@ -151,8 +165,12 @@ export default function Header() {
             </div>
           </div>
           <div className="flex-1 md:flex-none flex h-full md:px-[10px]">
-            <div className='flex-1 flex itmes-center justify-center'>
-              {accessToken  ?  <LogoutBtn /> : <LoginBtn openLoginModal={openLoginModal}/>}
+            <div className="flex justify-center flex-1 itmes-center">
+              {accessToken ? (
+                <LogoutBtn />
+              ) : (
+                <LoginBtn openLoginModal={openLoginModal} />
+              )}
             </div>
             <button
               className="md:text-xl md:cursor-pointer flex md:hover:text-[#efda7a] text-end"
@@ -161,7 +179,7 @@ export default function Header() {
               {!darkMode && (
                 <FontAwesomeIcon
                   icon={faMoon}
-                  className='text-[20px] m-[10px]'
+                  className="text-[20px] m-[10px]"
                   style={{
                     paddingRight: "7px",
                   }}
@@ -170,23 +188,19 @@ export default function Header() {
               {darkMode && (
                 <FontAwesomeIcon
                   icon={faSun}
-                  className='text-[20px] m-[10px]'
+                  className="text-[20px] m-[10px]"
                   style={{
                     paddingRight: "7px",
                   }}
                 />
               )}
             </button>
-            
           </div>
         </div>
       </div>
       <div className=" w-full flex md:hidden bg-white items-center shadow-sm shadow-slate-200 group-focus-within:shadow-blue-400 hover:shadow-blue-400 mb-[5px]">
         <label htmlFor="smallSearch" className="ml-[10px]">
-          <FontAwesomeIcon
-            icon={faSearch}
-            style={{ paddingRight: "7px" }}
-          />
+          <FontAwesomeIcon icon={faSearch} style={{ paddingRight: "7px" }} />
         </label>
         <input
           type="text"
