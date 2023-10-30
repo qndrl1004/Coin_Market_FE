7a0f2df68736ef30a3f181e0bad7708c99891c58
qndrl1004 <qndrl1004@gmail.com>
2023-10-30 11:47:01 +0900

    frontend 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    // host: "0.0.0.0", // 이곳에 원하는 IP 주소를 설정하세요.
-    // port: 5174,
     proxy: {
       "/api": {
-        target: "https://port-0-coin-market-be-12fhqa2llob5p0if.sel5.cloudtype.app",
+        target:
+          "https://port-0-coin-market-be-12fhqa2llob5p0if.sel5.cloudtype.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
